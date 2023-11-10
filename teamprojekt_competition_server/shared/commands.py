b1f3ba7eaacd8d17804f52310a976034f74e680c
Paul Masan <me@paulmasan.com>
2023-11-11 00:00:30 +0100

    deleted the example, added DOCString

--- teamprojekt_competition_server/shared/commands.py
+++ teamprojekt_competition_server/shared/commands.py
@@ -4,14 +4,39 @@ from twisted.protocols.amp import Integer, String, Boolean, Command, AmpList
 
 
 class AuthFailed(Exception):
+    """Exception raised when authentication fails.
+
+    This exception is raised in cases where the authentication process encounters
+    an issue, such as an invalid token.
+
+    Attributes:
+        None
+    """
     pass
 
 
 class InvalidVersion(Exception):
+    """Exception raised for an invalid version during authentication.
+
+    This exception is raised when the version provided during the authentication
+    process is not compatible with the current version of the server.
+
+    Attributes:
+        None
+    """
     pass
 
 
 class AuthClient(Command):
+    """[CLIENT] Command for authenticating the client with the server.
+
+    Arguments:
+        token (String): Token of the client
+        version (Integer): Version number of the running client framework 
+    
+    Response:
+        uuid (Integer): UUID assigned by the server to the client
+    """
     arguments: [(b"token", String()), (b"version", Integer())]
     response: [(b"uuid", Integer())]
     fatalErrors: {AuthFailed: "invalid-token", InvalidVersion: "invalid-version"}
@@ -21,11 +46,15 @@ class AuthClient(Command):
 
 
 class StartGame(Command):
+    """[SERVER] Command to notify the client that the game starts
+    """
     arguments: [(b"game_id", Integer())]
     response: [(b"ready", Boolean())]
 
 
 class EndGame(Command):
+    """[SERVER] Command to notify the client that the game has ended
+    """
     arguments: [(b"result", Boolean()), (b"stats", AmpList())]
     response: [(b"ready", Boolean())]
 
@@ -46,5 +75,7 @@ class EndGame(Command):
 
 
 class Step(Command):
+    """[SERVER] Command for requesting the next step from the agent
+    """
     arguments: [(b"env", AmpList())]
     response: [(b"action", AmpList())]
