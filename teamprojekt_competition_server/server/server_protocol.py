ef08bf84a8b109feb85817a29b20751e19cf6707
Paco Schatz <paco.schatz25@gmx.de>
2023-11-20 15:56:50 +0100

    Added a simle list to keep track of logged in clients

--- teamprojekt_competition_server/server/server_protocol.py
+++ teamprojekt_competition_server/server/server_protocol.py
@@ -1,4 +1,3 @@
-from collections import deque
 from twisted.internet.interfaces import IAddress
 from twisted.protocols import amp
 from twisted.internet.protocol import Protocol, ServerFactory
@@ -17,7 +16,17 @@ from game import Game
 class COMPServerProtocol(amp.AMP):
     game = None
 
+    def connectionLost(self, reason):
+        """ handle everythin necessary when a client disconnects"""
+        self.factory.client_disconnected(self)
+        return super().connectionLost(reason)
+
     def auth_client(self, token, version):
+        # TODO some auth handeling here, before we log the client as active
+
+        self.factory.client_connected(
+            self
+        )  # log active clients after they authenticated
         print(f"--- Authentification --- \nToken: {token} | Version: {version}")
         self.factory.find_opponent(self)
         return {"uuid": 1111}  # dummy uuid
@@ -41,7 +50,8 @@ class COMPServerProtocol(amp.AMP):
 
 class COMPServerFactory(ServerFactory):
     protocol = COMPServerProtocol
-    queue = deque()  # queue for storing agents waiting for a game
+    player_queue = list()  # queue for storing agents waiting for a game
+    active_clients = list()  # a basic list for storing logged in clients
 
     def buildProtocol(self, addr: IAddress) -> Protocol | None:
         protocol = COMPServerProtocol()
@@ -49,11 +59,26 @@ class COMPServerFactory(ServerFactory):
         return protocol
 
     def find_opponent(self, player: COMPServerProtocol):
-        if len(self.queue) < 1:  # no players waiting
-            self.queue.append(player)
+        if len(self.player_queue) < 1:  # no players waiting
+            self.player_queue.append(player)
         else:
             player1 = player
-            player2 = self.queue.pop()
+            player2 = self.player_queue.pop()
             game = Game(player1=player1, player2=player2)
             player1.start_game(game)
             player2.start_game(game)
+
+    def client_connected(self, client):
+        """ add a newly connected client to the list of logged in clients"""
+        self.active_clients.append(client)  # add new client
+        # print('a client connected to the server.' )
+        # print('currently there are ' + str(len(self.active_clients)) + ' active clients:')
+
+    def client_disconnected(self, client):
+        """ remove a disconnected client from the list of logged in clients"""
+        try:
+            self.active_clients.remove(client)  # try to remove disconnected client
+        except ValueError:
+            pass
+        # print('a client disconnected.' )
+        # print('currently there are ' + str(len(self.active_clients)) + ' active clients:')
