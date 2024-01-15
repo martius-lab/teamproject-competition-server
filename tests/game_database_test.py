fa6e7b0d5ec85ccf99d499e5441683510d95b032
Carina Straub <carina.s@online.de>
2024-01-15 16:56:16 +0100

    formatting

--- tests/game_database_test.py
+++ tests/game_database_test.py
@@ -1,7 +1,10 @@
 import teamprojekt_competition_server.server.game_database as game_db
 import logging
 
+# run with python -m tests.game_database_test
+
 logging.basicConfig(level=logging.DEBUG)
+
 
 def database_tests():
     gameID1 = game_db.insert_won_game(
@@ -19,20 +22,23 @@ def database_tests():
         score_disconnected_user=6,
         score_other_user=7,
     )
-    
+
     print(gameID1, gameID2, gameID3)
-    assert gameID1 % 3 == 1 and gameID2 % 3 == 2 and gameID3 % 3 == 0 
+    assert gameID1 % 3 == 1 and gameID2 % 3 == 2 and gameID3 % 3 == 0
     assert game_db.get_user_ids(game_id=gameID2) == (43, 23)
-    assert game_db.win_rate(user_id=23) == 0.5
-    assert len(game_db.get_game_ids(user_id=23)) % 3 == 0
-    assert len(game_db.get_won_game_ids(user_id=23)) == len(game_db.get_game_ids(user_id=23))/3
-    assert game_db.count_played_games(user_id=23) % 3 == 0
-    assert game_db.count_won_games(user_id=23) == game_db.count_played_games(user_id=23)/3
+
+    won_games = game_db.get_won_game_ids(user_id=23)
+    all_games = game_db.get_game_ids(user_id=23)
+    assert len(all_games) % 3 == 0
+    assert len(won_games) == len(all_games) / 3
+
+    assert game_db.count_played_games(user_id=23) == len(all_games)
+    assert game_db.count_won_games(user_id=23) == len(won_games)
     assert game_db.count_disconnected_games(user_id=23) == 0
-    assert game_db.count_games_with_disconnect(user_id=23) == game_db.count_played_games(user_id=23)/3
-    
+    assert game_db.count_games_with_disconnect(user_id=23) == len(all_games) / 3
+    assert game_db.win_rate(user_id=23) == 0.5
+
 
 if __name__ == "__main__":
-    database_tests() # only enable for manual testing
-
-
+    # database_tests()  # only enable for manual testing
+    pass
