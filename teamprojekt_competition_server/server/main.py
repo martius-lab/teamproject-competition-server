03c73cb930dddc869423eb0fa3a337deea6eea38
Paul Masan <me@paulmasan.com>
2024-01-22 16:14:33 +0100

    removed database test from main

--- teamprojekt_competition_server/server/main.py
+++ teamprojekt_competition_server/server/main.py
@@ -47,19 +47,3 @@ def main():
     """main function for testing"""
     server = COMPServer(LaserHockeyGame)
     server.start()
-
-
-def test_database():
-    """function to test database"""
-    game_db = GameDatabase()
-    gameID = game_db.insert_game(12, 23)
-    gameID = game_db.insert_game(52, 12)
-    print(gameID)
-    print(game_db.get_playerIDs(gameID=gameID))
-    print(game_db.get_gameIDs(playerID=12))
-    print(game_db.get_won_gameIDs(playerID=12))
-
-
-if __name__ == "__main__":
-    main()
-    # test_database()
