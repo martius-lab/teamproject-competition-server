9ca7f6f9fd01e696a8e5cef08c11be407e96ebca
Svea <svea.gaenzle@gmx.de>
2024-02-06 11:20:53 +0100

    Added a new folder scripts and a script to add new users to the user database

---
+++ comprl/scripts/new_user.py
@@ -0,0 +1,28 @@
+import comprl.server.user_database as user_db
+import logging
+import uuid
+
+# run with python -m comprl.scripts.new_user
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def insert_user(name: str):
+    """inserts a user to the user_db with a random token and a given name
+
+    Args:
+        name (str): name of the new user
+    """
+    user_db.add_user(user_name=name, user_token=uuid.uuid4())
+
+
+if __name__ == "__main__":
+    while True:
+        name = input(
+            "Please enter a name for the user or press ENTER to end the script: "
+        )
+        if name:
+            insert_user(name)
+        else:
+            break
+    pass
