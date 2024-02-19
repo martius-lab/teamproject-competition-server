34aa0ed559fae13a2aab3854217f81015cfa3af9
Svea <svea.gaenzle@gmx.de>
2024-02-19 21:14:20 +0100

    added a script to add dummy user to the user database

---
+++ comprl/scripts/dummy_user.py
@@ -0,0 +1,22 @@
+""" script to add dummy user to the user database"""
+
+import comprl.server.user_database as user_db
+import logging
+import uuid
+
+# run with python -m comprl.scripts.dummy_user
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def insert_users():
+    """inserts four dummy user to the user_db"""
+    user_db.add_user(user_name="test1", user_token="token1")
+    user_db.add_user(user_name="test2", user_token="token2")
+    user_db.add_user(user_name="test3", user_token="token3")
+    user_db.add_user(user_name="test4", user_token="token4")
+
+
+if __name__ == "__main__":
+    insert_users()
+    pass
