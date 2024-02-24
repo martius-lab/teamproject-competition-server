58260f2a23387a749ba8e0186b3e69ba7dbf1fc6
Svea <svea.gaenzle@gmx.de>
2024-02-24 15:31:01 +0100

    updated the skript using the new project structure

--- comprl/scripts/dummy_user.py
+++ comprl/scripts/dummy_user.py
@@ -1,6 +1,7 @@
 """ script to add dummy user to the user database"""
 
-import comprl.server.user_database as user_db
+from comprl.server.data import UserData
+from comprl.server.util import ConfigProvider
 import logging
 
 # run with python -m comprl.scripts.dummy_user
@@ -9,11 +10,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def insert_users():
-    """inserts four dummy user to the user_db"""
-    user_db.add_user(user_name="test1", user_token="token1")
-    user_db.add_user(user_name="test2", user_token="token2")
-    user_db.add_user(user_name="test3", user_token="token3")
-    user_db.add_user(user_name="test4", user_token="token4")
+    """inserts four dummy user to the user database"""
+    user_data = UserData(ConfigProvider.get("user_data"))
+    user_data.add(user_name="test1", user_token="token1")
+    user_data.add(user_name="test2", user_token="token2")
+    user_data.add(user_name="test3", user_token="token3")
+    user_data.add(user_name="test4", user_token="token4")
 
 
 if __name__ == "__main__":
