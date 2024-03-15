ce00a4e9154e4cf06da6d14794ffe16e10dd1777
Paul Masan <me@paulmasan.com>
2024-03-15 18:25:33 +0100

    added ui for game search, missing sql query

--- comprl-web/app/routes/_dashboard.tsx
+++ comprl-web/app/routes/_dashboard.tsx
@@ -70,7 +70,7 @@ export default function DashboardLayout() {
   );
 
   const data = useLoaderData<typeof loader>()
-  console.log(data.popup)
+  //console.log(data.popup)
 
   return (
     <Box sx={{ display: 'flex' }}>
