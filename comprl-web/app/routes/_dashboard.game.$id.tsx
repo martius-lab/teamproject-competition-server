4db4a3e4d77b877f65be661bbbccc7312aa127c9
Carina Straub <carina.s@online.de>
2024-03-15 19:39:56 +0100

    use GameResult interface

--- comprl-web/app/routes/_dashboard.game.$id.tsx
+++ comprl-web/app/routes/_dashboard.game.$id.tsx
@@ -3,24 +3,19 @@ import { Box, Button, Divider, Grid, Paper, Stack, TextField, ThemeProvider, Typ
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { DashboardPaper } from "~/components/DashboardContent";
-import { getGame, getUserName } from "~/db/sqlite.data";
+import { getGame, getUsername } from "~/db/sqlite.data";
 import { themeOptions } from "~/style/theme";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const game_id = params.id
   const game = await getGame(game_id)
 
-  if (!game) { return { id: game_id, game: null, username1: null, username2: null } }
-
-  const username1 = await getUserName(game.user1)
-  const username2 = await getUserName(game.user2)
-
-  return { id: game_id, game: game, username1: username1, username2: username2 };
+  return { id: game_id, game: game };
 }
 
 
 export default function Game() {
-  const { id, game, username1, username2 } = useLoaderData<typeof loader>();
+  const { id, game } = useLoaderData<typeof loader>();
 
   if (!game) { return (<Typography> No game with id {id} could be found.  </Typography>) }
   const theme = createTheme(themeOptions);
@@ -52,7 +47,7 @@ export default function Game() {
       <Paper variant="outlined" sx={{ bgcolor: 'primary.light', borderColor: 'primary.main' }} >
         <Grid container justifyContent={'center'} spacing={{xs: 0, md:2}} columnSpacing={{xs: 0, md:3}}>
           <Grid item xs={12} md={4} lg={5}>
-            <Typography variant="h5" color={'primary.main'} padding={2} sx={{textAlign: {xs: "center", md:"right"}}}> {username1} </Typography>
+            <Typography variant="h5" color={'primary.main'} padding={2} sx={{textAlign: {xs: "center", md:"right"}}}> {game.participants[0].name} </Typography>
           </Grid>
           <Grid item xs={12} md={1} lg={1}>
             <Typography 
@@ -66,12 +61,12 @@ export default function Game() {
               padding={2} 
               borderColor={'primary.main'} 
               color={'primary.main'}>
-              {game.score1}  -  {game.score2}
+              {game.participants[0].score}  -  {game.participants[1].score}
             </Typography>
           </Grid>
           <Divider orientation="vertical" variant="fullWidth" />
           <Grid item xs={12} md={4} lg={5}>
-            <Typography variant="h5" color={'primary.main'} padding={2} sx={{textAlign: {xs: "center", md:"left"}}}> {username2} </Typography>
+            <Typography variant="h5" color={'primary.main'} padding={2} sx={{textAlign: {xs: "center", md:"left"}}}> {game.participants[1].name} </Typography>
           </Grid>
         </Grid>
         <Typography variant="h5"> {
@@ -83,8 +78,14 @@ export default function Game() {
             <Typography variant="h5" marginBottom={2}> Game Information </Typography>
             <Typography variant="body1"> Time Stamp: {game.start_time} </Typography>
             <Typography variant="body1"> Game End: {gameEnd} </Typography>
-            {game.winner && <Typography variant="body1"> Winner: {game.winner==game.user1? username1 : username2} </Typography>}
-            {game.disconnected && <Typography variant="body1"> Disconnected Player: {game.disconnected==game.user1? username1 : username2} </Typography>}
+            {game.participants.map( (participant, i) => {
+              if (participant.winner) return <Typography variant="body1"> Winner: {participant.name} </Typography>
+              return null
+            })}
+            {game.participants.map( (participant, i) => {
+              if (participant.disconnected) return <Typography variant="body1"> Winner: {participant.name} </Typography>
+              return null
+            })}
           </DashboardPaper>
         </Grid>
         <Grid item xs={12} md={6}>
