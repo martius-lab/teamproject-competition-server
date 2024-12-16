9b013fb51769aeff6a51657465ba66ee9b5b1470
Felix Kloss <felix.kloss@uni-tuebingen.de>
2024-12-16 12:23:54 +0100

    Copy blog example for user registration/login
    Taken from
    https://dev.to/emmakodes_/setup-user-auth-for-your-reflex-app-using-localauth-4nj8
    
    For not just plain copy&paste without modifications.

--- comprl-web-reflex/comprl_web/comprl_web.py
+++ comprl-web-reflex/comprl_web/comprl_web.py
@@ -5,10 +5,9 @@ import reflex as rx
 from rxconfig import config
 
 
-class State(rx.State):
-    """The app state."""
-
-    ...
+from .base_state import State
+from .login import require_login
+from .registration import registration_page
 
 
 def index() -> rx.Component:
@@ -22,11 +21,7 @@ def index() -> rx.Component:
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
+            rx.link("Protected Page", href="/protected"),
             spacing="5",
             justify="center",
             min_height="85vh",
@@ -35,5 +30,25 @@ def index() -> rx.Component:
     )
 
 
+@require_login
+def protected() -> rx.Component:
+    """Render a protected page.
+
+    The `require_login` decorator will redirect to the login page if the user is
+    not authenticated.
+
+    Returns:
+        A reflex component.
+    """
+    return rx.vstack(
+        rx.heading(
+            "Protected Page for ", State.authenticated_user.username, font_size="2em"
+        ),
+        rx.link("Home", href="/"),
+        rx.link("Logout", href="/", on_click=State.do_logout),
+    )
+
+
 app = rx.App()
 app.add_page(index)
+app.add_page(protected)
--- comprl-web-reflex/rxconfig.py
+++ comprl-web-reflex/rxconfig.py
@@ -2,4 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="comprl_web",
+    db_url="sqlite:///reflex.db",
 )
