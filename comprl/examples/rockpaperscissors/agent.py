ac14310ac6baf64e706c24487d9ce8ddea28a238
Felix Kloss <felix.kloss@uni-tuebingen.de>
2024-12-17 10:24:29 +0100

    Adapt example agents of simple and rockpaperscissors games
    Derive a custom agent class instead of using the decorator and use the
    `launch_client` function.
    
    This is in preparation of making IAgent an abstract class, which will
    not allow the use of the `event` decorator anymore.

--- comprl/examples/rockpaperscissors/agent.py
+++ comprl/examples/rockpaperscissors/agent.py
@@ -1,27 +1,23 @@
-from comprl.client import Agent
 import random
+from comprl.client import Agent, launch_client
 
 
-agent = Agent()
+class MyAgent(Agent):
+    """An Rock-Paper-Scissor agent."""
+
+    def get_step(self, obv: list[float]):
+        return [float(random.randint(0, 2))]
+
+    def on_start_game(self, game_id: int):
+        print("game started")
+
+    def on_end_game(self, result, stats):
+        text_result = "won" if result else "lost"
+        print(
+            f"game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
 
 
-@agent.event
-def get_step(obv: list[float]):
-    return [float(random.randint(0, 2))]
-
-
-@agent.event
-def on_start_game(game_id: int):
-    print("game started")
-
-
-@agent.event
-def on_end_game(result, stats):
-    text_result = "won" if result else "lost"
-    print(
-        f"game ended: {text_result} with my score: "
-        f"{stats[0]} against the opponent with score: {stats[1]}"
-    )
-
-
-agent.run("token" + str(input("enter 1, 2, 3 or 4 to choose token: ")))
+if __name__ == "__main__":
+    launch_client(lambda args: MyAgent())
